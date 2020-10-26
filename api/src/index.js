9979a1c4a9254da2ceca7291b68593a89021ea63
Gevert Rico <devGevert@gmail.com>
2020-10-26 11:00:55 -0500

    Estructura del proyecto y arranques iniciales de funcionalidad.

---
+++ api/src/index.js
@@ -0,0 +1,1 @@
+console.log('Servicio del juego three inline');
---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Juego 3 en Línea</h1>
+    </div>
+  );
+}
+
+export default App;
